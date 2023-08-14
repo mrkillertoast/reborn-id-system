cabf9cdd6ff1c1d21a030ace2d282ff93abc15e8
mrkillerotast <lueku.rauscher@gmail.com>
2023-08-14 10:18:52 +0200

    - changed layout - integrated config file

--- webview/RebornIdSystem.vue
+++ webview/RebornIdSystem.vue
@@ -1,12 +1,37 @@
 <template>
     <div class="id-card">
+        <span class="id-card-header"
+            ><center>{{ IDSystemConfig.TRANSLATIONS.ID_CARD_HEADER }}</center>
+        </span>
         <div class="id-card-elements">
             <div class="id-card-picture">
-                <div class="placeholder-image"></div>
+                <img src="/plugins/avatar-placeholder.png" width="150" height="150" />
             </div>
             <div class="id-card-text">
-                <h2>Joe Doe</h2>
-                <p>Random City / 29.06.2023</p>
+                <div class="id-card-grid">
+                    <div class="id-card-text-element id-card-text-state">
+                        {{ IDSystemConfig.TRANSLATIONS.LAST_NAME }}:
+                    </div>
+                    <div class="id-card-text-element id-card-text-pinfo">Wilson</div>
+                    <div class="id-card-text-element id-card-text-state">
+                        {{ IDSystemConfig.TRANSLATIONS.FIRST_NAME }}
+                    </div>
+                    <div class="id-card-text-element id-card-text-pinfo">Stan</div>
+                    <div class="id-card-text-element id-card-text-state">
+                        {{ IDSystemConfig.TRANSLATIONS.DATE_OF_BIRTH }}
+                    </div>
+                    <div class="id-card-text-element id-card-text-pinfo">10.10.1000</div>
+                    <div class="id-card-text-element id-card-text-state">
+                        {{ IDSystemConfig.TRANSLATIONS.GENDER }}
+                    </div>
+                    <div class="id-card-text-element id-card-text-pinfo">Male</div>
+                </div>
+            </div>
+        </div>
+        <div class="id-card-footer id-card-grid">
+            <div class="id-card-signature">John Doe</div>
+            <div class="id-card-licences">
+                <p></p>
             </div>
         </div>
     </div>
@@ -14,15 +39,8 @@
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-// @ts-ignore
 import WebViewEvents from '@ViewUtility/webViewEvents';
-
-let test = ref('hello world');
-let count = ref(0);
-
-function clickButtonEvent() {
-    count.value += 1;
-}
+import { IDSystemConfig } from '../shared/config';
 
 // You can also make a function that emits to the server from the WebView
 // function emitToServer() {
@@ -40,27 +58,58 @@ onMounted(() => {
 
 <style scoped>
 .id-card {
-    color: red;
+    color: white;
     position: absolute;
     top: 0;
     left: 0;
     margin: 2%;
     background-color: green;
-    width: 20vw;
-    height: 15vh;
+    border-radius: 1rem;
+    width: 25vw;
+    height: 23vh;
+}
+.id-card > .id-card-header {
+    color: white;
+    font-style: italic;
+    font-size: small;
+}
+.id-card > .id-card-header > center {
+    margin-top: 2.2%;
+    padding: 1%;
+    background-color: blue;
 }
 
 .id-card > .id-card-elements {
+    margin-top: 2%;
     display: grid;
-    grid-template-columns: 1fr 2fr;
-    align-items: center;
-    height: 15vh;
+    grid-template-columns: 0.75fr 1fr;
+    align-items: start;
+}
+
+.id-card-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: start;
 }
 
 .id-card > .id-card-elements > .id-card-picture {
-    width: 5vw;
-    height: 10vh;
-    background-color: red;
-    margin: 15%;
+    justify-self: center;
+}
+
+.id-card-text-element {
+    padding: 7% 0%;
+}
+
+.id-card-text-state {
+    font-weight: bold;
+    text-transform: uppercase;
+}
+
+.id-card-signature {
+    font-size: 2rem;
+    font-style: italic;
+    margin: 2% 0 0 10%;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
 </style>
+../shared/config
