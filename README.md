dfd9c9a668a0f851d372a748a3d7f08de9cc239d
mrkillerotast <lueku.rauscher@gmail.com>
2023-08-14 10:18:26 +0200

    - add translations file to easy change text - updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # reborn-id-system
+
 Simple System to display ID Cards and to other Players
+
+### configuration
+
+Find the config.ts file inside the Shared-folder to modify ID Card textes.
---
+++ shared/config.ts
@@ -0,0 +1,12 @@
+export const IDSystemConfig = {
+    TRANSLATIONS: {
+        ID_CARD_HEADER: 'Los Santos Citizen Administration | State of San Andreas',
+        ID_CARD_NUMBER: 'No.',
+        LAST_NAME: 'Lastname',
+        FIRST_NAME: 'Firstname',
+        DATE_OF_BIRTH: 'Date of Birth',
+        DATE_OF_ISSUE: 'Date of Issue',
+        GENDER: 'SEX',
+        OPTAINED_LICENSES: 'Optained Licenses',
+    },
+};
