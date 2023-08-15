e6ef2253fca7000223c07dee6a568837d6a80850
mrkillerotast <lueku.rauscher@gmail.com>
2023-08-15 21:07:34 +0200

    - add custom event - add item version number

--- server/src/items.ts
+++ server/src/items.ts
@@ -1,5 +1,7 @@
+import { RebornIdSystemEvents } from '@AthenaPlugins/reborn-id-system/shared/viewEvents';
 import { BaseItem } from '@AthenaShared/interfaces/item';
 import * as Athena from '@AthenaServer/api';
+import { IDSystemConfig } from '@AthenaPlugins/reborn-id-system/shared/config';
 
 export const registerItem = async () => {
     await Athena.systems.inventory.factory.upsertAsync(defaultIdCard);
@@ -29,4 +31,11 @@ const defaultIdCard: BaseItem = {
         isEquippable: false,
         isWeapon: false,
     },
+    customEventsToCall: [
+        {
+            name: IDSystemConfig.ITEM.CUSTOM_EVENT_NAME,
+            eventToCall: RebornIdSystemEvents.ServerClient.OPEN_CITYHALL_WEBVIEW,
+        },
+    ],
+    version: 1,
 };
